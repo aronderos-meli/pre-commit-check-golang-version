0e7086a88dbf2c547dce099ac0bf7e686e7494fe
aronderos-meli <alvaro.ronderos@mercadolibre.com.co>
2022-02-02 17:44:20 -0500

    V0.1.0 (#1)
    * first commit
    
    * add sh

---
+++ check-go-version.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    echo "Please add $(args: [version]) in your pre-commit config"
+    exit 1
+fi
+
+base_version=$1
+current_version=$(grep -w 'go.mod' -e '^go [1-3].[1-9]*$' | awk -F' ' '{print $2}')
+check_version=$(awk -v b="${base_version}" -v c="${current_version}" 'BEGIN {if (b <= c) print "true"; else print "false"}')
+
+if [[ $check_version == "true" ]]; then
+    echo "go version ${current_version} is OK"
+else
+    echo "go version ${current_version} is minor to required ${base_version}"
+    exit 1
+fi
